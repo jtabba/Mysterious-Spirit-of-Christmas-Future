105e04621adfde5e7642500c506b97e7b7611177
jt <jtabba@hotmail.com>
2023-04-20 20:43:52 +1000

    altered webpack files to better differentiate prod and dev

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,7 +1,53 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.config.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
-module.exports = merge(common, {
+module.exports = {
 	mode: "production",
-	devtool: "source-map"
-});
+	target: "web",
+	devtool: "source-map",
+	entry: "./src/App.tsx",
+	output: {
+		path: path.resolve(__dirname, "build"),
+		publicPath: "auto",
+		filename: "bundle.js"
+	},
+	devServer: {
+		static: path.join(__dirname, "src/"),
+		port: 3000,
+		hot: "only",
+		compress: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+			favicon: "src/logo/logo-color.svg"
+		}),
+		new Dotenv()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				loader: "ts-loader"
+			},
+			{
+				test: /(\.css)$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.svg$/,
+				use: "file-loader"
+			}
+		]
+	},
+	resolve: {
+		extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
+	}
+};
