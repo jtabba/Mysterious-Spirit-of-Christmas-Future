3264d8118e07142339617d0bbf94fcdbd3677da3
jt <jtabba@hotmail.com>
2023-04-25 21:31:34 +1000

    env bug fix

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
 
 module.exports = merge(common, {
 	mode: "production",
@@ -10,6 +11,12 @@ module.exports = merge(common, {
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: "[name].[contenthash].css"
+		}),
+		new webpack.DefinePlugin({
+			API_URL: JSON.stringify(process.env.API_URL),
+			ENCRYPTION_PASSPHRASE: JSON.stringify(
+				process.env.ENCRYPTION_PASSPHRASE
+			)
 		})
 	],
 	optimization: {
